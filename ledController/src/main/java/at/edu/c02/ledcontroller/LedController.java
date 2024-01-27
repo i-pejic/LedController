d7cc5fb827316daa6af7f163af6ab15b93c767af
Matthias <herzer.matthias@gmail.com>
2024-01-27 09:45:47 +0100

    Implemented requested functions as per Story Instructions

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedController.java
@@ -3,5 +3,8 @@ package at.edu.c02.ledcontroller;
 import java.io.IOException;
 
 public interface LedController {
+    String[] getGroupLeds() throws Exception;
+
     void demo() throws IOException;
 }
+
--- ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * This class handles the actual logic
@@ -11,14 +12,33 @@ import java.io.IOException;
 public class LedControllerImpl implements LedController {
     private final ApiService apiService;
 
-    public LedControllerImpl(ApiService apiService)
-    {
+    public LedControllerImpl(ApiService apiService) {
         this.apiService = apiService;
     }
 
     @Override
-    public void demo() throws IOException
-    {
+    public String[] getGroupLeds() throws Exception {
+        JSONObject lightsResponse = apiService.getLights();
+        JSONArray lightsArray = lightsResponse.getJSONArray("lights");
+
+        // Extract information for each light and add to result
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < lightsArray.length(); i++) {
+            JSONObject light = lightsArray.getJSONObject(i);
+            int id = light.getInt("id");
+            String color = light.getString("color");
+            boolean isGrouped = light.getBoolean("isGrouped");  // Annahme: Annahme, dass "isGrouped" ein boolean-Wert ist.
+
+            // Add information to the result ArrayList
+            result.add("Light ID: " + id + ", Color: " + color + ", Grouped: " + isGrouped);
+        }
+
+        // Convert the ArrayList to an array
+        return result.toArray(new String[0]);
+    }
+
+    @Override
+    public void demo() throws IOException {
         // Call `getLights`, the response is a json object in the form `{ "lights": [ { ... }, { ... } ] }`
         JSONObject response = apiService.getLights();
         // get the "lights" array from the response
